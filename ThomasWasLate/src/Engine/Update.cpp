7d283eeb13d0fef3cc807a7694a9a13365a94cd1
msearles25 <skaman4459@gmail.com>
2020-11-02 10:52:29 -0500

    spawn logic in update method for the engine

--- ThomasWasLate/src/Engine/Update.cpp
+++ ThomasWasLate/src/Engine/Update.cpp
@@ -3,6 +3,17 @@
 
 void Engine::update(float dtAsSeconds)
 {
+	if (m_NewLevelRequired)
+	{
+		// Spawn Thomas and Bob
+		m_Thomas.spawn(sf::Vector2f(0, 0), GRAVITY);
+		m_Bob.spawn(sf::Vector2f(0, 100), GRAVITY);
+
+		// Make sure our spawn is only called one time
+		m_TimeRemaining = 10;
+		m_NewLevelRequired = false;
+	}
+
 	if (m_Playing)
 	{
 		// Count down the amount of time the player has left
