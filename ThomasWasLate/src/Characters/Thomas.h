593673090c70a83d25126382ba65a8e885b4a195
msearles25 <skaman4459@gmail.com>
2020-11-02 10:15:02 -0500

    Thomas class

---
+++ ThomasWasLate/src/Characters/Thomas.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "../PlayableCharacter/PlayableCharacter.h"
+
+// Inheritance from PlayableCharacter
+class Thomas : public PlayableCharacter
+{
+public:
+	// Constructor specific to only Thomas
+	Thomas();
+
+	// The overridden input handler for Thomas
+	bool virtual handleInput();
+};
