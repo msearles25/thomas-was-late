ad2b04dba99baf9ccef428057f8a15bd3ded4ded
msearles25 <skaman4459@gmail.com>
2020-11-02 07:53:59 -0500

    basics of the engine input method

---
+++ ThomasWasLate/src/Engine/Input.cpp
@@ -0,0 +1,36 @@
+#include "Engine.h"
+
+void Engine::input()
+{
+	sf::Event event;
+	
+	while (m_Window.pollEvent(event))
+	{
+		if (event.type == sf::Event::KeyPressed)
+		{
+			// Handle the player quiting the game
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+			{
+				m_Window.close();
+			}
+
+			// Handle the player starting the game
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return))
+			{
+				m_Playing = true;
+			}
+
+			// Switching between Thomas and Bob
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+			{
+				m_Character1 = !m_Character1;
+			}
+
+			// Switch between fullscreen and split-screen
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::E))
+			{
+				m_SplitScreen = !m_SplitScreen;
+			}
+		}
+	}
+}
