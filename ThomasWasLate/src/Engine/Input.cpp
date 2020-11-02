54ba4541669ae4356f12e7949d47a4ea435ca6c0
msearles25 <skaman4459@gmail.com>
2020-11-02 10:41:47 -0500

    setting up inputhandles in the engine for thomas and bob for lateruse

--- ThomasWasLate/src/Engine/Input.cpp
+++ ThomasWasLate/src/Engine/Input.cpp
@@ -33,4 +33,15 @@ void Engine::input()
 			}
 		}
 	}
+
+	// Handle inputs specific to Thomas
+	if (m_Thomas.handleInput())
+	{
+		// We'll play a sound here
+	}
+	// Handle Inputs specific to Bob
+	if (m_Bob.handleInput())
+	{
+		// Again, we'll play a sound here
+	}
 }
