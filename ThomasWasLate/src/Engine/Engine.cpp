aa6b0e8fab0b4a1861455170bbb57ceec67eab5f
msearles25 <skaman4459@gmail.com>
2020-11-02 07:23:20 -0500

    engine constructor

---
+++ ThomasWasLate/src/Engine/Engine.cpp
@@ -0,0 +1,28 @@
+#include "Engine.h"
+
+Engine::Engine()
+{
+	// Get our screen resolution
+	// and create an SFML window and a view
+	sf::Vector2f resolution;
+	resolution.x = sf::VideoMode::getDesktopMode().width;
+	resolution.y = sf::VideoMode::getDesktopMode().height;
+
+	m_Window.create(sf::VideoMode(resolution.x, resolution.y), "Thomas was late");
+
+	// Initialize the fullscreen view
+	m_MainView.setSize(resolution);
+	m_HudView.reset(sf::FloatRect(0, 0, resolution.x, resolution.y));
+
+	// Initialize the split-screen view
+	m_LeftView.setViewport(sf::FloatRect(0.001f, 0.001f, 0.498f, 0.998f));
+	m_RightView.setViewport(sf::FloatRect(0.5f, 0.001f, 0.499f, 0.998f));
+
+	m_BGLeftView.setViewport(sf::FloatRect(0.001f, 0.001f, 0.498f, 0.998f));
+	m_BGRightView.setViewport(sf::FloatRect(0.5f, 0.001f, 0.499f, 0.998f));
+
+	m_BackgroundTexture = TextureHolder::GetTexture("Content/graphics/background.png");
+
+	// Associate our texture with the sprite
+	m_BackgroundSprite.setTexture(m_BackgroundTexture);
+}
